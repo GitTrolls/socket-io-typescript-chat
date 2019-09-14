666b951ee10c8f6de24c4e8f86f340bb57f6c8c3
GitTrolls <theaaronbyte@gmail.com>
2019-09-14 23:16:02 +0300

    fixed issue with  TS2304: Cannot find name 'Symbol'

--- client/src/app/chat/shared/services/socket.service.ts
+++ client/src/app/chat/shared/services/socket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Observer } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 import { Message } from '../model/message';
 import { Event } from '../model/event';
 
